4359224e2822c1d6388499782c82f744bc608243
liter-alb-dev <liter.alb.dev@gmail.com>
2023-08-05 14:17:47 +0300

    wrote all migrations

--- database/migrations/2023_08_04_125146_create_tasks_table.php
+++ database/migrations/2023_08_04_125146_create_tasks_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('manager_id')->nullable(true)->default(NULL);
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->string('status');
+            $table->unsignedBigInteger('worker_id');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
