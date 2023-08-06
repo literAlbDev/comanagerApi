ffb1fd4a09199daeaf5348d06e8da0e9832c130e
liter-alb-dev <liter.alb.dev@gmail.com>
2023-08-06 20:14:02 +0300

    updated tasks table migration

--- database/migrations/2023_08_04_125146_create_tasks_table.php
+++ database/migrations/2023_08_04_125146_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->string('status');
+            $table->string('status')->default('pending');
             $table->unsignedBigInteger('worker_id');
             $table->timestamps();
         });
